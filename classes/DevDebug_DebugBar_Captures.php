cb8f8d32fab55bb20563e3d49f07f32a4daeec42
Evan Mattson <me@aaemnnost.tv>
2014-09-07 14:55:26 -0400

    only show capture panel if there are captures

--- classes/DevDebug_DebugBar_Captures.php
+++ classes/DevDebug_DebugBar_Captures.php
@@ -5,12 +5,10 @@ class DevDebug_DebugBar_Captures extends Debug_Bar_Panel
 {
 	function init()
 	{
-		ddprint(__METHOD__);
 		$this->api = DevDebug::get_instance();
 		$this->title( sprintf('DevDebug Captures <span class="count">%s</span>', $this->api->capture_count()) );
+		$this->_visible = $this->api->has_captures();
 	}
-
-	function prerender() {}
 
 	function render()
 	{
