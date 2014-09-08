2209179c0fe2479b0561d48a7c0e289759a378ba
Evan Mattson <me@aaemnnost.tv>
2014-09-08 00:15:06 -0400

    set capture panel title and visibility in prerender

--- classes/DevDebug_DebugBar_Captures.php
+++ classes/DevDebug_DebugBar_Captures.php
@@ -6,6 +6,10 @@ class DevDebug_DebugBar_Captures extends Debug_Bar_Panel
 	function init()
 	{
 		$this->api = DevDebug::get_instance();
+	}
+
+	function prerender()
+	{
 		$this->title( sprintf('DevDebug Captures <span class="count">%s</span>', $this->api->capture_count()) );
 		$this->_visible = $this->api->has_captures();
 	}
